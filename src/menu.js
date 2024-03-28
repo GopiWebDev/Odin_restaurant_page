fb72c27cf4faec546c8d5bf4819f6921cc4c28ed
GopiWebDev <gopisv.webdev@gmail.com>
2024-03-28 14:54:45 +0530

    imported images and added source to all images

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,10 @@
+import Deluxe from "./images/Deluxe.jpg";
+import Extravaganz from "./images/Extravaganz.jpg";
+import Farmhouse from "./images/Farmhouse.jpg";
+import Mexican from "./images/Mexican.jpg";
+import Peppy from "./images/Peppy.jpg";
+import Veggie from "./images/Veggie.jpg";
+
 const createMenu = () => {
   const content = document.querySelector("#content");
 
@@ -7,49 +14,55 @@ const createMenu = () => {
   menu.appendChild(
     createMenuItem(
       "Farmhouse",
-      " Apizza that goes ballistic on veggies! Check out this mouth watering overload of crunchy, crisp capsicum, succulent mushrooms and fresh tomatoes"
+      " Apizza that goes ballistic on veggies! Check out this mouth watering overload of crunchy, crisp capsicum, succulent mushrooms and fresh tomatoes",
+      Farmhouse
     )
   );
 
   menu.appendChild(
     createMenuItem(
       "Peppy",
-      "Chunky paneer with crisp capsicum and spicy red pepper - quite a mouthful!"
+      "Chunky paneer with crisp capsicum and spicy red pepper - quite a mouthful!",
+      Peppy
     )
   );
 
   menu.appendChild(
     createMenuItem(
       "Mexican",
-      "A pizza loaded with crunchy onions, crisp capsicum, juicy tomatoes and jalapeno with a liberal sprinkling of exotic Mexican herbs."
+      "A pizza loaded with crunchy onions, crisp capsicum, juicy tomatoes and jalapeno with a liberal sprinkling of exotic Mexican herbs.",
+      Mexican
     )
   );
 
   menu.appendChild(
     createMenuItem(
       "Deluxe",
-      "For a vegetarian looking for a BIG treat that goes easy on the spices, this one's got it all.. The onions, the capsicum, those delectable mushrooms - with paneer and golden corn to top it all."
+      "For a vegetarian looking for a BIG treat that goes easy on the spices, this one's got it all.. The onions, the capsicum, those delectable mushrooms - with paneer and golden corn to top it all.",
+      Deluxe
     )
   );
 
   menu.appendChild(
     createMenuItem(
       "Extravaganz",
-      "A pizza that decidedly staggers under an overload of golden corn, exotic black olives, crunchy onions, crisp capsicum, succulent mushrooms, juicyfresh tomatoes and jalapeno - with extra cheese to go all around."
+      "A pizza that decidedly staggers under an overload of golden corn, exotic black olives, crunchy onions, crisp capsicum, succulent mushrooms, juicyfresh tomatoes and jalapeno - with extra cheese to go all around.",
+      Extravaganz
     )
   );
 
   menu.appendChild(
     createMenuItem(
       "Veggie",
-      "Goldern Corn, Black Olives, Capsicum & Red Paprika"
+      "Goldern Corn, Black Olives, Capsicum & Red Paprika",
+      Veggie
     )
   );
 
   content.appendChild(menu);
 };
 
-const createMenuItem = (name, description) => {
+const createMenuItem = (name, description, imagesrc) => {
   const menuItem = document.createElement("div");
   menuItem.classList.add("item");
 
@@ -60,7 +73,7 @@ const createMenuItem = (name, description) => {
   foodDescription.textContent = description;
 
   const foodImg = document.createElement("img");
-  foodImg.src = `/src/images/${name}.jpg`;
+  foodImg.src = imagesrc;
 
   menuItem.appendChild(foodImg);
   menuItem.appendChild(foodName);
