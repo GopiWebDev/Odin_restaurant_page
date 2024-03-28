08fae486425716db601bfb3ca1e71dabb7b94712
GopiWebDev <gopisv.webdev@gmail.com>
2024-03-28 14:21:27 +0530

    404 error while loading assets

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,13 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[hash][ext][query]",
+        },
+      },
     ],
   },
 };
